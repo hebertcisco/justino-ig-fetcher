d208ba27c1e46f0168eb1301859e92936495891a
hebertcisco <hebertcisco@outlook.com>
2022-01-23 13:18:47 -0300

    feat: :sparkles: add global constants

---
+++ src/shared/constants/global.ts
@@ -0,0 +1,4 @@
+export const INSTAGRAM_BASE_URL = 'https://www.instagram.com/';
+
+export const HASHTAGS_REGEX_PATTERN = /(?<=[\s>])#(\d*[A-Za-z_]+\d*)\b(?!;)/g;
+export const USERNAMES_REGEX_PATTERN = /@([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_]))?)/g;
